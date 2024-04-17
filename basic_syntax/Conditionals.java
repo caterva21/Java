c9407187815ef9cb72140d051d8b8a450223913d
Xubuntu <139293883.caterva21.users.noreply.github.com>
2024-04-17 00:20:07 -0300

    This is program compares the length of the name with the number of characters the user thought.

---
+++ basic_syntax/Conditionals.java
@@ -0,0 +1,28 @@
+/* @brief it compares the length of the name with the number of characters the user thought.
+ * @details Scanner is instantiated to read the user's input and We use length() method.
+ * @file conditionals.java
+ * @date 16/04/20214
+ * @version 1.0*/
+
+import java.util.Scanner;
+public class Conditionals {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.next();
+        System.out.print("How many characters do you think your name has: ");
+        int number_of_characters = scanner.nextInt();
+
+        int name_length = name.length(); 
+
+        if (name_length == number_of_characters) {
+            System.out.println("The length of the name is equal to the numeric of characters you thought");
+        } else if (name_length > number_of_characters) {
+            System.out.println("The length of the name is greater than the numeric of characters you thought");
+        } else {
+            System.out.println("The length of the name is less than the numeric of characters you thought");
+        }
+        scanner.close();
+    }
+}
