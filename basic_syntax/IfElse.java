c7d416158204027eab157f8e4509e7a9ce10fef2
Anthony  Bañon <139293883+caterva21@users.noreply.github.com>
2024-04-17 00:42:08 -0300

    Update and rename Conditionals.java to IfElse.java
    Clear name

--- basic_syntax/IfElse.java
+++ basic_syntax/IfElse.java
@@ -5,7 +5,7 @@
  * @version 1.0*/
 
 import java.util.Scanner;
-public class Conditionals {
+public class IfElse {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
